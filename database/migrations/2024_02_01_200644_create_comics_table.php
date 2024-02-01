069c4a0199ff56ddd80a9d368c9910c9f868fc17
JohnQuimson <john.quimson777@gmail.com>
2024-02-01 21:16:44 +0100

    feat: migration

---
+++ database/migrations/2024_02_01_200644_create_comics_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title', 80);
+            $table->string('description', 1000);
+            $table->string('thumb', 500);
+            $table->string('price', 10);
+            $table->string('series', 30);
+            $table->date('sale_date');
+            $table->string('type', 30);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comics');
+    }
+};
